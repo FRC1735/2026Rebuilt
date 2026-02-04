ba62eeabfd79c8053ef144f6622e358c00d1200e
Jared Andrews <jared@jaredandrews.com>
2026-02-03 21:47:38 -0500

    Add Advantage Kit TalonFX(S) Swerve Template, required changes documented on the website are already completed.

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2026Rebuilt";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "7aaa6d4b6a54e2a1d9fc322dd58af424eb7f4209";
+  public static final String GIT_DATE = "2026-02-03 21:46:45 EST";
+  public static final String GIT_BRANCH = "master";
+  public static final String BUILD_DATE = "2026-02-03 21:47:19 EST";
+  public static final long BUILD_UNIX_TIME = 1770173239978L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
