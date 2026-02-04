c17bfe7a39467812161f27eba6851702e5cb2703
Jared Andrews <jared@jaredandrews.com>
2026-02-03 22:18:09 -0500

    Update PathPlanner code to use our previously created settings

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 13;
-  public static final String GIT_SHA = "ba62eeabfd79c8053ef144f6622e358c00d1200e";
-  public static final String GIT_DATE = "2026-02-03 21:47:38 EST";
+  public static final int GIT_REVISION = 14;
+  public static final String GIT_SHA = "9c735a1e241a8324404d2eb81d559c1472727ee9";
+  public static final String GIT_DATE = "2026-02-03 22:11:08 EST";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2026-02-03 22:10:27 EST";
-  public static final long BUILD_UNIX_TIME = 1770174627307L;
+  public static final String BUILD_DATE = "2026-02-03 22:17:34 EST";
+  public static final long BUILD_UNIX_TIME = 1770175054205L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
