3745839906bc7fa68c4471773c3f63cf541c63d8
Jared Andrews <jared@jaredandrews.com>
2026-02-05 10:36:51 -0500

    Move limelight stuff into dedicated folder

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 14;
-  public static final String GIT_SHA = "9c735a1e241a8324404d2eb81d559c1472727ee9";
-  public static final String GIT_DATE = "2026-02-03 22:11:08 EST";
+  public static final int GIT_REVISION = 20;
+  public static final String GIT_SHA = "13f928ed159159d6ffb8e2999859bdb9db2e3953";
+  public static final String GIT_DATE = "2026-02-04 13:28:30 EST";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2026-02-03 22:17:34 EST";
-  public static final long BUILD_UNIX_TIME = 1770175054205L;
+  public static final String BUILD_DATE = "2026-02-05 10:36:00 EST";
+  public static final long BUILD_UNIX_TIME = 1770305760805L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
