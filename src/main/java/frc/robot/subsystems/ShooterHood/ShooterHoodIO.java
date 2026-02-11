e958ab0c9d0fe5586d7f9ef9327f5a3192e6d486
green-reaper-1735 <jared+1735@jaredandrews.com>
2026-02-10 20:25:33 -0500

    Partial work for shooter hood

---
+++ src/main/java/frc/robot/subsystems/ShooterHood/ShooterHoodIO.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems.ShooterHood;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ShooterHoodIO {
+
+    @AutoLog
+    class ShooterHoodIOInputs {
+        public double velocityRadPerSec = 0.0;
+
+        public double appliedVolts = 0.0;
+
+        public double currentAmps = 0.0;
+        
+        public boolean connected = true;
+    }
+
+  default void updateInputs(ShooterHoodIOInputs inputs) {}
+
+  default void setVoltage(double volts) {}
+
+  default void stop() {
+    setVoltage( 0.0);
+  }
+}
