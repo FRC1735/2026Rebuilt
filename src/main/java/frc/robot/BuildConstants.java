bfeda52cf7407547664530fb99a81dd902b381be
Jared Andrews <jared@jaredandrews.com>
2026-02-20 19:36:46 -0500

    Import files for listening to XK-80 keyboard

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 62;
-  public static final String GIT_SHA = "15c62f9bb70290117cf67e3f51a33d127717576a";
-  public static final String GIT_DATE = "2026-02-18 12:42:30 EST";
+  public static final int GIT_REVISION = 63;
+  public static final String GIT_SHA = "41583fd1a6106a9117967bbabf9152e416518be9";
+  public static final String GIT_DATE = "2026-02-18 14:02:34 EST";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2026-02-18 13:59:40 EST";
-  public static final long BUILD_UNIX_TIME = 1771441180023L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-02-20 18:53:57 EST";
+  public static final long BUILD_UNIX_TIME = 1771631637634L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
