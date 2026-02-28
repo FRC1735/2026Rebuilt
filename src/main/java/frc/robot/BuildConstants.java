c67fae71aa078bd325413e41ce318e347b7861e0
green-reaper-1735 <jared+1735@jaredandrews.com>
2026-02-28 12:59:14 -0500

    Switched back to comp tuner constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 73;
-  public static final String GIT_SHA = "0e039263c331a433678cc44b4933358ee2b73a6f";
-  public static final String GIT_DATE = "2026-02-28 11:27:34 EST";
+  public static final int GIT_REVISION = 74;
+  public static final String GIT_SHA = "2913e8ae2b52e54d7297468e343a218ddb319e68";
+  public static final String GIT_DATE = "2026-02-28 12:55:55 EST";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2026-02-28 12:54:35 EST";
-  public static final long BUILD_UNIX_TIME = 1772301275945L;
+  public static final String BUILD_DATE = "2026-02-28 12:58:53 EST";
+  public static final long BUILD_UNIX_TIME = 1772301533044L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
