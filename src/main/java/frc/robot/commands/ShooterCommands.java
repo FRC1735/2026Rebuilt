408206a7a74fcedff9519d53e7d98f512f811c89
green-reaper-1735 <jared+1735@jaredandrews.com>
2026-03-06 17:50:20 -0500

    DELETE debug shooter commands

--- src/main/java/frc/robot/commands/ShooterCommands.java
+++ src/main/java/frc/robot/commands/ShooterCommands.java
@@ -31,36 +31,6 @@ public class ShooterCommands {
   }
 
   public static Command shooterHighSpeed(Shooter shooter, RollerIntake shooterIntake) {
-    /*return Commands.runOnce(shooter::shootHigh, shooter)
-    .until(() -> shooter.atTargetVelocity())
-    .andThen(Commands.run(shooterIntake::in));*/
-
-    /*
-    return new StartEndCommand(
-        () ->
-            Commands.run(shooter::shootHigh, shooter)
-                .until(() -> shooter.atTargetVelocity())
-                .andThen(shooterIntake::in, shooterIntake),
-        shooter::stop,
-        shooter,
-        shooterIntake);
-        */
-
-    /*
-    return new StartEndCommand(
-        () -> {
-          Commands.run(shooter::shootHigh, shooter)
-              .until(() -> shooter.atTargetVelocity())
-              .andThen(shooterIntake::in, shooterIntake);
-        },
-        () ->
-            new ParallelCommandGroup(
-                Commands.run(shooter::stop, shooter),
-                Commands.run(shooterIntake::stop, shooterIntake)),
-        shooter,
-        shooterIntake);
-        */
-
     return Commands.run(shooter::shootHigh, shooter)
         .until(shooter::atTargetVelocity)
         .andThen(Commands.run(shooterIntake::in, shooterIntake))
