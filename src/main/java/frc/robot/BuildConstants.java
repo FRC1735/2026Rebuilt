f1d636980c78d63909e1a34d961fae8aa3dff2cc
green-reaper-1735 <jared+1735@jaredandrews.com>
2026-03-14 10:00:59 -0400

    Tunable PID in shooter

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 118;
-  public static final String GIT_SHA = "7d10314e0788ea718fda26ac26a0fefc26107788";
-  public static final String GIT_DATE = "2026-03-14 09:04:53 EDT";
+  public static final int GIT_REVISION = 119;
+  public static final String GIT_SHA = "4a48e9e4d66a4a1eeed724ababe76cbe9ee98707";
+  public static final String GIT_DATE = "2026-03-14 09:35:13 EDT";
   public static final String GIT_BRANCH = "new_shooter";
-  public static final String BUILD_DATE = "2026-03-14 09:31:03 EDT";
-  public static final long BUILD_UNIX_TIME = 1773495063739L;
+  public static final String BUILD_DATE = "2026-03-14 09:53:19 EDT";
+  public static final long BUILD_UNIX_TIME = 1773496399050L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/subsystems/shooter/ShooterIOSparkFlex.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOSparkFlex.java
@@ -10,6 +10,7 @@ import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkFlexConfig;
+import edu.wpi.first.networktables.DoubleEntry;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import frc.robot.Constants;
@@ -23,6 +24,17 @@ public class ShooterIOSparkFlex implements ShooterIO {
 
   private double targetVelocity = 0;
 
+  // Tunables
+  private double p = 0;
+  private double i = 0;
+  private double d = 0;
+
+  private final DoubleEntry pEntry;
+  private final DoubleEntry iEntry;
+  private final DoubleEntry dEntry;
+
+  private double lastP, lastI, lastD;
+
   // Dashboard keys
   private static final String kPrefix = "Shooter/";
 
@@ -33,6 +45,14 @@ public class ShooterIOSparkFlex implements ShooterIO {
     pid = leader.getClosedLoopController();
 
     NetworkTable table = NetworkTableInstance.getDefault().getTable("Elastic").getSubTable(kPrefix);
+
+    pEntry = table.getDoubleTopic("P").getEntry(p);
+    iEntry = table.getDoubleTopic("I").getEntry(i);
+    dEntry = table.getDoubleTopic("D").getEntry(d);
+
+    pEntry.set(p);
+    iEntry.set(i);
+    dEntry.set(d);
 
     configureMotors();
   }
@@ -48,9 +68,9 @@ public class ShooterIOSparkFlex implements ShooterIO {
     leadConfig
         .closedLoop
         .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-        .p(0)
-        .i(0)
-        .d(0)
+        .p(p)
+        .i(i)
+        .d(d)
         .outputRange(-1, 1)
         .feedForward
         .kV((0.5 / 2722) * 10);
@@ -61,10 +81,27 @@ public class ShooterIOSparkFlex implements ShooterIO {
         followerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
     leader.configure(leadConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    lastP = p;
+    lastI = i;
+    lastD = d;
+  }
+
+  private void checkForChanges() {
+
+    p = pEntry.get();
+    i = iEntry.get();
+    d = dEntry.get();
+
+    if (p != lastP || i != lastI || d != lastD) {
+      configureMotors();
+    }
   }
 
   @Override
   public void updateInputs(ShooterIOInputs inputs) {
+    checkForChanges();
+
     inputs.encoderVelocity = encoder.getVelocity();
     inputs.targetVelocity = targetVelocity;
 
