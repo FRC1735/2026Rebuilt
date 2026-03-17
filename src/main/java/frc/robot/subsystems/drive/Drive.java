59ce5c1835652bed27e9eaa59b0aaaea3da22b4e
green-reaper-1735 <jared+1735@jaredandrews.com>
2026-03-17 18:26:30 -0400

    make field global var

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -63,7 +63,7 @@ public class Drive extends SubsystemBase {
           Math.max(
               Math.hypot(TunerConstants.BackLeft.LocationX, TunerConstants.BackLeft.LocationY),
               Math.hypot(TunerConstants.BackRight.LocationX, TunerConstants.BackRight.LocationY)));
-
+    Field2d field = new Field2d();
   // PathPlanner config constants
   // TODO - Remember to update these for real robot
   private static final double ROBOT_MASS_KG = 18.144;
@@ -207,7 +207,7 @@ public class Drive extends SubsystemBase {
       // Apply update
       poseEstimator.updateWithTime(sampleTimestamps[i], rawGyroRotation, modulePositions);
     }
-    Field2d field = new Field2d();
+
     field.setRobotPose(poseEstimator.getEstimatedPosition());
     SmartDashboard.putData("field", field);
     if (OperatorToggles.isVisionEnabled()) {
