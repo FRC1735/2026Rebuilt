7f8ca398b86b06542ad7e36641a5c49fce8b7323
green-reaper-1735 <jared+1735@jaredandrews.com>
2026-03-20 17:43:04 -0400

    changed shooter hood setpoints and offsets

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 156;
-  public static final String GIT_SHA = "365f6e671103e0411e53b320fe24947db924134c";
-  public static final String GIT_DATE = "2026-03-20 14:30:27 EDT";
+  public static final int GIT_REVISION = 157;
+  public static final String GIT_SHA = "599eab3a491e4b4b532a9d3203650e10a76baded";
+  public static final String GIT_DATE = "2026-03-20 14:33:13 EDT";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2026-03-20 14:31:47 EDT";
-  public static final long BUILD_UNIX_TIME = 1774031507979L;
+  public static final String BUILD_DATE = "2026-03-20 17:41:29 EDT";
+  public static final long BUILD_UNIX_TIME = 1774042889903L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
