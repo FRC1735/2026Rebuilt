5ac6864c8a02c17b130857379e4f07fc275e2902
green-reaper-1735 <jared+1735@jaredandrews.com>
2026-04-02 18:59:14 -0400

    adjust elastic dashboard

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 172;
-  public static final String GIT_SHA = "f2c40d6760a24aed55934730b3c9790320aeaba9";
-  public static final String GIT_DATE = "2026-03-31 19:24:23 EDT";
+  public static final int GIT_REVISION = 173;
+  public static final String GIT_SHA = "c02815f08751b21a2ca5276b02742bccb0bdf9fc";
+  public static final String GIT_DATE = "2026-03-31 20:23:15 EDT";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2026-03-31 20:01:41 EDT";
-  public static final long BUILD_UNIX_TIME = 1775001701245L;
+  public static final String BUILD_DATE = "2026-04-02 18:46:57 EDT";
+  public static final long BUILD_UNIX_TIME = 1775170017934L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -33,7 +33,7 @@ public final class Constants {
   public static final boolean SHOOTER_INTAKE_ENABLED = false;
   /// Collector
   public static final boolean COLLECTOR_DEPLOYER_ENABLED = false;
-  public static final boolean COLLECTOR_ROLLER_EXTERIOR_ENABLED = true;
+  public static final boolean COLLECTOR_ROLLER_EXTERIOR_ENABLED = false;
 
   // CAN ID's
   /// Shooter
