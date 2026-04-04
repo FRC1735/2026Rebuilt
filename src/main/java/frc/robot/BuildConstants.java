97ee0aaa837330ea1bdcd29b468345d72e8a9642
green-reaper-1735 <jared+1735@jaredandrews.com>
2026-04-04 11:26:32 -0400

    new hood offset

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 180;
-  public static final String GIT_SHA = "721eabb252decc79e86a93be4fc96cad23804905";
-  public static final String GIT_DATE = "2026-04-04 08:46:17 EDT";
+  public static final int GIT_REVISION = 183;
+  public static final String GIT_SHA = "227852065bcce0536f19ee609a3f5882d7149a05";
+  public static final String GIT_DATE = "2026-04-04 11:10:51 EDT";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2026-04-04 09:04:59 EDT";
-  public static final long BUILD_UNIX_TIME = 1775307899756L;
+  public static final String BUILD_DATE = "2026-04-04 11:23:12 EDT";
+  public static final long BUILD_UNIX_TIME = 1775316192357L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/subsystems/ShooterHood/ShooterHoodIOSparkFlex.java
+++ src/main/java/frc/robot/subsystems/ShooterHood/ShooterHoodIOSparkFlex.java
@@ -89,7 +89,7 @@ public class ShooterHoodIOSparkFlex implements ShooterHoodIO {
         .absoluteEncoder
         .positionConversionFactor(1.0)
         .velocityConversionFactor(1.0)
-        .zeroOffset(0.9)
+        .zeroOffset(0.01)
         .inverted(true);
 
     config
