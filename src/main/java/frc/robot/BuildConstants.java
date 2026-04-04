a782d6379c91420b4726f6f54df6f84eed99932b
green-reaper-1735 <jared+1735@jaredandrews.com>
2026-04-04 12:48:21 -0400

    changed deploy setpoints

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 187;
-  public static final String GIT_SHA = "dd0830535dab6589b36e61d003034555acbc8406";
-  public static final String GIT_DATE = "2026-04-04 11:47:50 EDT";
+  public static final int GIT_REVISION = 188;
+  public static final String GIT_SHA = "fd3cf5c484a39f8c9cc433592f1dd167c428580e";
+  public static final String GIT_DATE = "2026-04-04 12:11:37 EDT";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2026-04-04 12:10:51 EDT";
-  public static final long BUILD_UNIX_TIME = 1775319051384L;
+  public static final String BUILD_DATE = "2026-04-04 12:46:29 EDT";
+  public static final long BUILD_UNIX_TIME = 1775321189684L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
