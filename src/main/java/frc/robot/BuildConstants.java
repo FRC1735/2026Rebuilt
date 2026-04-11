cdae6ac70f96b7aa7c4726f9cfb4bc59c8d5c621
green-reaper-1735 <jared+1735@jaredandrews.com>
2026-04-11 13:30:57 -0400

    tweak path planner turn PID

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 218;
-  public static final String GIT_SHA = "28f559c2568bfe7eaa92eb64a7dd21605e30a767";
-  public static final String GIT_DATE = "2026-04-11 12:03:50 EDT";
+  public static final int GIT_REVISION = 219;
+  public static final String GIT_SHA = "a312a0198bc72f326664ec98beb37d37c443065c";
+  public static final String GIT_DATE = "2026-04-11 13:12:59 EDT";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2026-04-11 13:09:21 EDT";
-  public static final long BUILD_UNIX_TIME = 1775927361603L;
+  public static final String BUILD_DATE = "2026-04-11 13:20:00 EDT";
+  public static final long BUILD_UNIX_TIME = 1775928000590L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -128,7 +128,7 @@ public class Drive extends SubsystemBase {
         this::getChassisSpeeds,
         this::runVelocity,
         new PPHolonomicDriveController(
-            new PIDConstants(4.0, 0, 0.125), new PIDConstants(1.5, 0.1, 0.0)),
+            new PIDConstants(4.0, 0, 0.125), new PIDConstants(2.5, 0.1, 0.0)),
         PP_CONFIG,
         () -> DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red,
         this);
