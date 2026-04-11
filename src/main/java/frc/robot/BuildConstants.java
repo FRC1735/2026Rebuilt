a312a0198bc72f326664ec98beb37d37c443065c
green-reaper-1735 <jared+1735@jaredandrews.com>
2026-04-11 13:12:59 -0400

    adjusting auto

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 217;
-  public static final String GIT_SHA = "b8284b094987385fe5571acacd6c8c6a67bdc803";
-  public static final String GIT_DATE = "2026-04-11 11:16:18 EDT";
+  public static final int GIT_REVISION = 218;
+  public static final String GIT_SHA = "28f559c2568bfe7eaa92eb64a7dd21605e30a767";
+  public static final String GIT_DATE = "2026-04-11 12:03:50 EDT";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2026-04-11 12:01:30 EDT";
-  public static final long BUILD_UNIX_TIME = 1775923290525L;
+  public static final String BUILD_DATE = "2026-04-11 13:09:21 EDT";
+  public static final long BUILD_UNIX_TIME = 1775927361603L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
