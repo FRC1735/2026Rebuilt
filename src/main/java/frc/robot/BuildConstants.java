c7aa0a38fc5dd176946d4e1e795d3ee85a78781d
green-reaper-1735 <45584689+green-reaper-1735@users.noreply.github.com>
2026-04-11 08:51:17 -0400

    updated bconst, akit, and phx6 software

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 193;
-  public static final String GIT_SHA = "e637de71d0800525f875f1782f269f09647be627";
-  public static final String GIT_DATE = "2026-04-07 20:28:08 EDT";
+  public static final int GIT_REVISION = 202;
+  public static final String GIT_SHA = "5baf5bc4200234b9cfd8af1ffa96946a5cb3c748";
+  public static final String GIT_DATE = "2026-04-07 20:31:51 EDT";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2026-04-07 20:31:34 EDT";
-  public static final long BUILD_UNIX_TIME = 1775608294235L;
+  public static final String BUILD_DATE = "2026-04-09 18:51:32 EDT";
+  public static final long BUILD_UNIX_TIME = 1775775092585L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
