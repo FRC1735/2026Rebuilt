7270b7194c139fa7170e49794758fe949a65f924
green-reaper-1735 <45584689+green-reaper-1735@users.noreply.github.com>
2026-04-16 12:25:03 -0400

    New commands for close / open

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 252;
-  public static final String GIT_SHA = "17f6a62d5a7411ef7dfbb7886d97f913e19ae37b";
-  public static final String GIT_DATE = "2026-04-16 12:15:51 EDT";
+  public static final int GIT_REVISION = 254;
+  public static final String GIT_SHA = "e52605baac6d62d540921d2be0ca49c4e04b2bc5";
+  public static final String GIT_DATE = "2026-04-16 12:23:17 EDT";
   public static final String GIT_BRANCH = "maintain_collector_position";
-  public static final String BUILD_DATE = "2026-04-16 12:21:22 EDT";
-  public static final long BUILD_UNIX_TIME = 1776356482678L;
+  public static final String BUILD_DATE = "2026-04-16 12:24:41 EDT";
+  public static final long BUILD_UNIX_TIME = 1776356681632L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
