df850bc17bf1e75936bd39dbe4e2472582f9fed0
green-reaper-1735 <45584689+green-reaper-1735@users.noreply.github.com>
2026-04-16 12:22:38 -0400

    functions to keep collector open / closed

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 251;
-  public static final String GIT_SHA = "84c1e4a2e4b27021af510734f7aebc939b11d263";
-  public static final String GIT_DATE = "2026-04-16 11:58:17 EDT";
+  public static final int GIT_REVISION = 252;
+  public static final String GIT_SHA = "17f6a62d5a7411ef7dfbb7886d97f913e19ae37b";
+  public static final String GIT_DATE = "2026-04-16 12:15:51 EDT";
   public static final String GIT_BRANCH = "maintain_collector_position";
-  public static final String BUILD_DATE = "2026-04-16 12:15:40 EDT";
-  public static final long BUILD_UNIX_TIME = 1776356140536L;
+  public static final String BUILD_DATE = "2026-04-16 12:21:22 EDT";
+  public static final long BUILD_UNIX_TIME = 1776356482678L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/subsystems/collectordeployer/CollectorDeployer.java
+++ src/main/java/frc/robot/subsystems/collectordeployer/CollectorDeployer.java
@@ -95,7 +95,6 @@ public class CollectorDeployer extends SubsystemBase {
   }
 
   public void scaledDeploy() {
-    // todo - not good
     double position = getPosition();
 
     // .26
@@ -135,6 +134,48 @@ public class CollectorDeployer extends SubsystemBase {
       setVoltage(-1);
     } else if (position > CollectorDeployer.CLOSED_TARGET) {
       setVoltage(-7.5);
+    } else {
+      setVoltage(0);
+    }
+  }
+
+  public void keepClosed() {
+    // this seems to be good
+    double position = getPosition();
+
+    if (position < CLOSED_TARGET + 0.04) {
+      setVoltage(0);
+      return;
+    }
+
+    // go slow
+    boolean goSlow =
+        position > CollectorDeployer.CLOSED_TARGET
+            && position < CollectorDeployer.CLOSED_TARGET + 0.15; // 0.15;
+    if (goSlow) {
+      setVoltage(-1);
+    } else {
+      setVoltage(0);
+    }
+  }
+
+  public void keepOpen() {
+    // todo - not good
+    double position = getPosition();
+
+    // .26
+
+    if (position > DEPLOYED_TARGET) {
+      setVoltage(0);
+      return;
+    }
+
+    // slow
+    boolean goSlow =
+        position < CollectorDeployer.DEPLOYED_TARGET + 0.1
+            && position > CollectorDeployer.DEPLOYED_TARGET - 0.28; // 0.125;
+    if (goSlow) {
+      setVoltage(1);
     } else {
       setVoltage(0);
     }
