c91be80b7c4a3cbc3465f979aaad20429356d10a
green-reaper-1735 <jared+1735@jaredandrews.com>
2026-05-05 19:54:58 -0400

    updated shooterHood setpoints

--- src/main/java/frc/robot/subsystems/ShooterHood/ShooterHoodIOSparkFlex.java
+++ src/main/java/frc/robot/subsystems/ShooterHood/ShooterHoodIOSparkFlex.java
@@ -89,7 +89,7 @@ public class ShooterHoodIOSparkFlex implements ShooterHoodIO {
         .absoluteEncoder
         .positionConversionFactor(1.0)
         .velocityConversionFactor(1.0)
-        .zeroOffset(0.01)
+        .zeroOffset(0.47)
         .inverted(true);
 
     config
