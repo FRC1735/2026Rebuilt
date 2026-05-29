ea141d5dc03b446a949d91fd7b8704bd919b2f12
green-reaper-1735 <jared+1735@jaredandrews.com>
2026-05-28 20:03:33 -0400

    Code to detect scoring phase

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 272;
-  public static final String GIT_SHA = "ce58aea6eea677f435ad3e61e03dd53456f9bbe6";
-  public static final String GIT_DATE = "2026-05-26 19:58:46 EDT";
+  public static final int GIT_REVISION = 273;
+  public static final String GIT_SHA = "a61fe75e3f1d6bedd363a48538b7a15770103518";
+  public static final String GIT_DATE = "2026-05-28 19:03:15 EDT";
   public static final String GIT_BRANCH = "battlecry";
-  public static final String BUILD_DATE = "2026-05-28 19:03:14 EDT";
-  public static final long BUILD_UNIX_TIME = 1780009394734L;
+  public static final String BUILD_DATE = "2026-05-28 20:02:12 EDT";
+  public static final long BUILD_UNIX_TIME = 1780012932083L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
