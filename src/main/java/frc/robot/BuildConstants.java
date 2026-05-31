0680caaf2fe6b742be9bcb1dc98d24eb903aeecd
green-reaper-1735 <jared+1735@jaredandrews.com>
2026-05-31 08:49:50 -0400

    added BC dot autos

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 281;
-  public static final String GIT_SHA = "2e054304ad479b2bd2f46bf99412d3788dd47553";
-  public static final String GIT_DATE = "2026-05-31 08:01:50 EDT";
+  public static final int GIT_REVISION = 282;
+  public static final String GIT_SHA = "0abe8de2857c3b08c48f5eb53c77756027b8e934";
+  public static final String GIT_DATE = "2026-05-31 08:10:01 EDT";
   public static final String GIT_BRANCH = "battlecry";
-  public static final String BUILD_DATE = "2026-05-31 08:07:11 EDT";
-  public static final long BUILD_UNIX_TIME = 1780229231746L;
+  public static final String BUILD_DATE = "2026-05-31 08:44:32 EDT";
+  public static final long BUILD_UNIX_TIME = 1780231472357L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
