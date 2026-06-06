8c576c94a29b1a4508766a9507b57caa0f21b25a
green-reaper-1735 <jared+1735@jaredandrews.com>
2026-06-05 21:14:22 -0400

    apply dot chooser to bc hub bd dot depot

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 291;
-  public static final String GIT_SHA = "75ff29a45d55a5ec4f4fa6fa9a6ace3b1fb991dc";
-  public static final String GIT_DATE = "2026-06-05 20:48:07 EDT";
-  public static final String GIT_BRANCH = "test_dot";
-  public static final String BUILD_DATE = "2026-06-05 20:56:10 EDT";
-  public static final long BUILD_UNIX_TIME = 1780707370525L;
+  public static final int GIT_REVISION = 292;
+  public static final String GIT_SHA = "06cca53d6f67b11bda2b9bbea32c84556e9c2ee2";
+  public static final String GIT_DATE = "2026-06-05 20:56:26 EDT";
+  public static final String GIT_BRANCH = "battlecry";
+  public static final String BUILD_DATE = "2026-06-05 21:11:48 EDT";
+  public static final long BUILD_UNIX_TIME = 1780708308148L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
